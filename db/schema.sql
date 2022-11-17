6d76d0f872f1cc95b68d584accd116a92acb5105
OilPainter1 <hjcryns@gmail.com>
2022-11-17 14:25:24 -0600

    fixed sql bugs

--- db/schema.sql
+++ db/schema.sql
@@ -3,31 +3,32 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE employees (
+CREATE TABLE employees(
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     /*Foreign Key*/
     roll_id INT,
     manager_id INT,
-    PRIMARY KEY (id)
-    FOREIGN KEY (roll_id) REFERENCES role(id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (manager_id) REFERENCES role_table(id)
     
 );
 
-CREATE TABLE role (
-    id INT NOT NULL,
+CREATE TABLE role_table(
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
-    PRIMARY KEY id,
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    PRIMARY KEY (id)
+
+    
     
 );
 
 CREATE TABLE department (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30),
-    PRIMARY KEY id
+    PRIMARY KEY (id)
 
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,5 @@
-INSERT INTO employee(id,first_name,last_name,roll_id,manager_id)
-VALUES (1,"John","Doe",32,54), (2,"Joe","Smith",32,10),(3,"James","Stark",45,10);
+INSERT INTO employees(id,first_name,last_name,roll_id,manager_id)
+VALUES (1,"John","Doe",32,2), (2,"Joe","Smith",1,10),(3,"James","Stark",45,10);
+
+INSERT INTO role_table(id,title,department_id)
+VALUES(32,"consultant",20),(1,"manager",1),(50,"counselor",2)
