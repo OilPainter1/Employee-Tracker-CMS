2a80933da17731b3c8afec561e5d4869a4824137
OilPainter1 <hjcryns@gmail.com>
2022-11-17 13:57:01 -0600

    added to schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -8,7 +8,26 @@ CREATE TABLE employees (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     /*Foreign Key*/
-    roll_id INT NOT NULL,
-    manager_id INT NOT NULL,
+    roll_id INT,
+    manager_id INT,
     PRIMARY KEY (id)
+    FOREIGN KEY (roll_id) REFERENCES role(id)
+    
+);
+
+CREATE TABLE role (
+    id INT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY id,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+    
+);
+
+CREATE TABLE department (
+    id INT NOT NULL,
+    name VARCHAR(30),
+    PRIMARY KEY id
+
 );
