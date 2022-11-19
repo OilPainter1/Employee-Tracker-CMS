2c3ad676cc89d0f6eead7ec0ed9350a5d44e77db
OilPainter1 <hjcryns@gmail.com>
2022-11-18 22:20:07 -0600

    created connection to mysql shell in index.js

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 require('dotenv').config()
-console.log(process.env)
-const mysql2 = require("mysql2")
-const inquirer = require("inquirer")
 
+const mysql2 = require("mysql2")
+//const inquirer = require("inquirer")
+
+const connection = mysql2.createConnection({
+    host: 'localhost',
+    user: process.env.db_username,
+    database: process.env.db_name
+  })
+
+const query1 = connection.promise().query(`insert into department values (?,?)`,[1,"HR"]).then(console.log("done"))
+ 
